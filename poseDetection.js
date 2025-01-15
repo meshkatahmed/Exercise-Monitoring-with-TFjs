6b2935995abfc26c1802e915e27c5260468d3f87
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2025-01-15 18:44:31 +0600

    Dummy API Integration

--- poseDetection.js
+++ poseDetection.js
@@ -33,6 +33,29 @@ const heightAlert = document.getElementById('height-alert');
 const instructionsContainer = document.getElementById('instructions-container');
 const boardContainer = document.getElementById('board-container');
 
+// Data Fetching through API
+const fetchData = async () => {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/api/details/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        console.log("Data from FastAPI:", data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
+fetchData();
+
 exerciseDropdown.addEventListener('change', () => {
     selectedExercise = exerciseDropdown.value;
     startButton.disabled = false;
